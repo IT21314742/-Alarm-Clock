6ba8160336f0f0744e0b48e48490934d0a637529
IT21314742 <it21314742@my.sliit.lk>
2025-01-05 22:49:56 +0530

    Update
    Update

--- clock.py
+++ clock.py
@@ -8,7 +8,6 @@ def set_alarm(alarm_time):
     print(f"Alarm set for {alarm_time}")
 
 
-
 if __name__ == "__main__": 
     alarm_time = input("Enter the alarm time (HH:MM:SS): ")
     set_alarm(alarm_time)
