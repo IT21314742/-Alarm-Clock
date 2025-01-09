46e70451b3294d6183cfcf12e4c3a1739fd019ae
IT21314742 <it21314742@my.sliit.lk>
2025-01-09 06:14:26 +0530

    Update
    Update

--- clock.py
+++ clock.py
@@ -21,7 +21,7 @@ def set_alarm(alarm_time):
         pygame.mixer.music.play()
 
         while pygame.mixer.music.get_busy():
-            time.sleep
+            time.sleep(1)
 
         is_running = False
 
